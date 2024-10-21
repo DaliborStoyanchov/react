3e185e01952f749ce6a3a5b9b6cc7224071200e5
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2024-10-21 15:42:08 +0200

    feat: set up appwrite db  connection; create a client

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <h1 className="text-4xl font-bold">Hello</h1>;
+  async function getSuggestions(params: any) {}
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Hello World!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </>
+  );
 };
 
 export default App;
---
+++ todo/src/lib/appwrite.ts
@@ -0,0 +1,11 @@
+import { Client, Databases } from "appwrite";
+
+const client = new Client();
+
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(import.meta.env.VITE_PROJECT_ID);
+
+export const databases = new Databases(client);
+export const DB_ID = import.meta.env.VITE_DB_ID;
+export const COLLECTION_ID = import.meta.env.VITE_COLLECTION_ID;
