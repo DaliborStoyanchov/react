24091217e9cad4410f98bfc2c36a910146f69c9b
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2024-10-22 15:28:56 +0200

    feat: add todos on render; add state for todos

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
+import { databases, DB_ID, COLLECTION_ID } from "./lib/appwrite";
+import * as appwrite from "appwrite";
+
+// interface ITodo {
+//   $id: string;
+//   text: string;
+//   isFinished: boolean;
+// }
+
 const App = () => {
-  async function getSuggestions(params: any) {}
+  const [todos, setTodos] = useState<appwrite.Models.Document[]>([]);
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  async function getTodos() {
+    const res = await databases.listDocuments(DB_ID, COLLECTION_ID);
+
+    const todos: appwrite.Models.Document[] = res.documents;
+
+    setTodos(todos);
+  }
 
   return (
-    <>
-      <h1 className="text-4xl font-bold">Hello World!</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-    </>
+    <main className="max-w-3xl w-11/12 mx-auto bg-slate-100 rounded p-10 mt-10">
+      <h1 className="text-4xl font-bold my-1 pb-10">To Do</h1>
+      <ul className="space-y-4">
+        {todos.map((todo) => (
+          <li
+            className="flex items-center border rounded border-black/20 shadow p-4 gap-2"
+            key={todo.$id}
+          >
+            {todo.text}
+            <span>{todo.isCompleted ? "✔" : null}</span>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 };
 
