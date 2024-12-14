c2144eb41daeee1827546756d10219b6a5ff4db0
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2024-12-14 13:22:16 +0100

    feat: add routing

--- movie-app/src/App.tsx
+++ movie-app/src/App.tsx
@@ -1,12 +1,20 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
 import Home from "./Pages/Home";
+import Favorites from "./Pages/Favorites";
+import Navbar from "./Components/NavBar";
 
 const App = () => {
   return (
-    <main className="App">
-      <h1>App</h1>
-      <Home />
-    </main>
+    <>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </>
   );
 };
 
