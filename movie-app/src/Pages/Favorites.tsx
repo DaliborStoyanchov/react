0346c2d62947bcff7560533a3b2a0e12762afde7
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2024-12-20 08:07:23 +0100

    fix: favorites context, feat: add favorites markup, add: styling

--- movie-app/src/Pages/Favorites.tsx
+++ movie-app/src/Pages/Favorites.tsx
@@ -1,6 +1,27 @@
 import classes from "../css/Favorites.module.css";
+import styles from "../css/Home.module.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../Components/MovieCard";
+import Movie from "../interface/IMovie";
 
 const Favorites = () => {
+  const { favorites } = useMovieContext();
+
+  console.log(favorites);
+
+  if (favorites.length > 0) {
+    return (
+      <div>
+        <h2 className={classes.favorites}>Your Favorite Movies</h2>
+        <div className={styles.moviesGrid}>
+          {favorites.map((movie: Movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={classes.favoritesEmpty}>
       <h2>No favorite movies yet</h2>
