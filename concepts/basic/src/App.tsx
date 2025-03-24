dec66367628c96b71051e09eb6b3074b7e477aca
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-03-24 10:20:16 +0100

    fix: change folder name

---
+++ concepts/basic/src/App.tsx
@@ -0,0 +1,16 @@
+// import Accordion from "./components/Accordion/Accordion";
+
+import RandomColor from "./components/RandomColor/RandomColor";
+
+const App = () => {
+  return (
+    <>
+      <main>
+        {/* <Accordion /> */}
+        <RandomColor />
+      </main>
+    </>
+  );
+};
+
+export default App;
