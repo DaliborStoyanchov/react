f4e90e76b8f5ceb41d720399efde2980d432eea8
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-04-11 21:43:02 +0200

    feat: custom hook useLocalStorage

--- concepts/basic/src/components/ThemeChange/ThemeChange.tsx
+++ concepts/basic/src/components/ThemeChange/ThemeChange.tsx
@@ -1,5 +1,18 @@
+import classes from "./ThemeChange.module.css";
+import useLocalStorage from "./useLocalStorage";
+
 const ThemeChange = () => {
-  return <div>ThemeChange</div>;
+  const [theme, setTheme] = useLocalStorage("theme", true);
+
+  return (
+    <div className={classes.lightDarkMode}>
+      <div className={classes.container}>
+        <h1>Light-Dark Mode</h1>
+        <p>Hello World</p>
+        <button>Change Theme</button>
+      </div>
+    </div>
+  );
 };
 
 export default ThemeChange;
