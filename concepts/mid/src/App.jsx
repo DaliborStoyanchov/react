6aaf62de2fcf3cd49bc90ffab69e23b902be38bc
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-04-19 16:11:05 +0200

    feat:styling

--- concepts/mid/src/App.jsx
+++ concepts/mid/src/App.jsx
@@ -1,11 +1,14 @@
 // import LoadMore from "./components/LoadMore/LoadMore";
 // import ModalTest from "./components/Modal/ModalTest";
 
+import GitHubProfileFinder from "./components/GitHubProfileFinder/GitHubProfileFinder";
+
 function App() {
   return (
     <>
       {/* <LoadMore /> */}
       {/* <ModalTest /> */}
+      <GitHubProfileFinder />
     </>
   );
 }
--- concepts/mid/src/components/GitHubProfileFinder/GitHubProfileFinder.jsx
+++ concepts/mid/src/components/GitHubProfileFinder/GitHubProfileFinder.jsx
@@ -1,20 +1,38 @@
+import { useEffect, useState } from "react";
 import classes from "./GitHubProfileFinder.module.css";
 
-const GitHubProfileFinder = () => {
+function GitHubProfileFinder() {
+  const [userName, setUserName] = useState("DaliborStoyanchov");
+  const [] = useState();
+
+  function handleSubmit() {}
+
+  async function fetchUserGithubData() {
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+
+    const data = await res.json();
+
+    console.log(data);
+  }
+
+  useEffect(() => {
+    fetchUserGithubData();
+  }, []);
+
   return (
     <div className={classes.gitHubProfileFinderContainer}>
       <div className={classes.inputWrapper}>
-        <form>
-          <input
-            type="text"
-            name="searchByUsername"
-            placeholder="Search Github Username"
-          />
-          <button>Search</button>
-        </form>
+        <input
+          type="text"
+          name="searchByUsername"
+          placeholder="Search Github Username"
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
       </div>
     </div>
   );
-};
+}
 
 export default GitHubProfileFinder;
