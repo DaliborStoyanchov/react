edeae2859ae2276102fa783bf8a22ca4fcf45afc
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-04-24 13:00:15 +0200

    feat:handle change show dropdown filter

--- concepts/mid/src/components/SearchAutocompleteAPI/SearchAutocompleteAPI.jsx
+++ concepts/mid/src/components/SearchAutocompleteAPI/SearchAutocompleteAPI.jsx
@@ -7,6 +7,26 @@ const SearchAutocompleteAPI = () => {
   const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
   const [error, setError] = useState(null);
+  const [searchParams, setSearchParams] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  function handleChange(e) {
+    const query = e.target.value.toLowerCase();
+
+    setSearchParams(query);
+
+    if (query.length > 1) {
+      const filteredData =
+        users && users.length
+          ? users.filter((item) => item.toLowerCase().indexOf(query) > -1)
+          : [];
+      setFilteredUsers(filteredData);
+      setShowDropdown(true);
+    } else {
+      setShowDropdown(false);
+    }
+  }
 
   async function fetchListOfUsers(url) {
     setLoading(true);
@@ -16,7 +36,7 @@ const SearchAutocompleteAPI = () => {
       const data = await response.json();
 
       if (data && data.users && data.users.length) {
-        setUsers(data.users);
+        setUsers(data.users.map((userItem) => userItem.firstName));
         setLoading(false);
         setError(null);
       }
@@ -31,9 +51,18 @@ const SearchAutocompleteAPI = () => {
     fetchListOfUsers(URL);
   }, []);
 
+  console.log(users);
+  console.log(filteredUsers);
+
   return (
     <div className={classes.searchAutoCompleteContainer}>
-      <input type="text" name="users" placeholder="Search users here..." />
+      <input
+        type="text"
+        name="users"
+        value={searchParams}
+        placeholder="Search users here..."
+        onChange={handleChange}
+      />
     </div>
   );
 };
