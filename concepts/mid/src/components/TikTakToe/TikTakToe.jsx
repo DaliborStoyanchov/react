0573465ac727a8ab84df0e46e0426734e5d8334f
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-04-28 13:00:16 +0200

    feat: styling

--- concepts/mid/src/components/TikTakToe/TikTakToe.jsx
+++ concepts/mid/src/components/TikTakToe/TikTakToe.jsx
@@ -1,5 +1,29 @@
+import module from "./TikTakToe.module.css";
+
+function Square({ value }) {
+  return <button className={module.square}>{value}</button>;
+}
+
 export function TikTakToe() {
-  return <div>TikTakToe</div>;
+  return (
+    <div className={module.container}>
+      <div className={module.row}>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className={module.row}>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className={module.row}>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
 }
 
 export default TikTakToe;
