23b1aa92daf2cb075664a64e4755ffedfc35170e
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-04-29 23:09:22 +0200

    feat: sqares logic and state

--- concepts/mid/src/components/TikTakToe/TikTakToe.jsx
+++ concepts/mid/src/components/TikTakToe/TikTakToe.jsx
@@ -1,26 +1,75 @@
+import { useEffect, useState } from "react";
 import module from "./TikTakToe.module.css";
 
-function Square({ value }) {
-  return <button className={module.square}>{value}</button>;
+function Square({ value, onClick }) {
+  return (
+    <button onClick={onClick} className={module.square}>
+      {value}
+    </button>
+  );
 }
 
 export function TikTakToe() {
+  const [squares, setSquares] = useState(Array(9).fill(""));
+  const [isXTurn, setIsXTurn] = useState(true);
+
+  function getWinner(squares) {
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+    ];
+
+    for (let i = 0; i < winningPatterns.length; i++) {
+      const [a, b, c] = winningPatterns[i];
+
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
+
+  function handleClick(currentSquare) {
+    console.log(currentSquare);
+
+    let cpySquares = [...squares];
+    if (cpySquares[currentSquare] || getWinner(cpySquares)) return;
+    cpySquares[currentSquare] = isXTurn ? "X" : "O";
+    setIsXTurn(!isXTurn);
+    setSquares(cpySquares);
+  }
+
+  useEffect(() => {}, [squares, isXTurn]);
+
+  console.log(squares);
+
   return (
     <div className={module.container}>
       <div className={module.row}>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className={module.row}>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className={module.row}>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </div>
   );
