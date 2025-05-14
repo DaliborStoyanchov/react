838c2253597385531bdc9761841f841a319722a7
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-05-14 20:33:11 +0200

    feat: search component

--- concepts/adv/components/weather-app/components/search/Search.jsx
+++ concepts/adv/components/weather-app/components/search/Search.jsx
@@ -1,5 +1,19 @@
-const Search = () => {
-  return <div>Search</div>;
+import classes from "./Search.module.css";
+
+const Search = ({ search, setSearch, handleSearch }) => {
+  return (
+    <div className={classes.searchEngine}>
+      <input
+        type="text"
+        className={classes.citySearch}
+        placeholder="Enter City Name"
+        name="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
 };
 
 export default Search;
