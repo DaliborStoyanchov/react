9e8a5355a58bb3917a70995475fbd7d68d2c50cb
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-05-15 18:01:52 +0200

    feat: api key, gitignore, evn

--- concepts/adv/components/weather-app/components/weather/Weather.jsx
+++ concepts/adv/components/weather-app/components/weather/Weather.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import Search from "../search/Search";
 
 const Weather = () => {
+  const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+
+  let url = "https://openweathermap.org/city/2643743";
+
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+
+  async function handleSearch() {}
+
   return (
     <div>
-      <Search />
+      <Search
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+      />
       Weather
     </div>
   );
