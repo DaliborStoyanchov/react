c3b52e17dbf05905a3903e4ceefb947668d03bd3
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-05-17 20:53:02 +0200

    feat: markup, get date

--- concepts/adv/components/weather-app/components/weather/Weather.jsx
+++ concepts/adv/components/weather-app/components/weather/Weather.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import Search from "../search/Search";
 
+import classes from "./Weather.module.css";
+
 const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
 const Weather = () => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("Skopje");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [weatherData, setWeatherData] = useState(null);
@@ -36,7 +38,18 @@ const Weather = () => {
     fetchWeatherData(search);
   }
 
-  useEffect(() => {}, []);
+  function getCurrentDate() {
+    return new Date().toLocaleString("en-us", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+  }
+
+  useEffect(() => {
+    fetchWeatherData("skopje");
+  }, []);
 
   return (
     <div>
@@ -45,8 +58,21 @@ const Weather = () => {
         setSearch={setSearch}
         handleSearch={handleSearch}
       />
-      {isLoading && <h3>Loading...</h3>}
-      {weatherData ? <p>{weatherData.name}</p> : <p>Enter City Name</p>}
+      {error && <p>{error}</p>}
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <div>
+          <div className={classes.cityName}>
+            <h3>
+              {weatherData?.name + ","} <span>{weatherData?.sys?.country}</span>
+            </h3>
+          </div>
+          <div className={classes.date}>
+            <span>{getCurrentDate()}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
