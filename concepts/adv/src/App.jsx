556ad4ea08deb68c3ec6d1ca405af7f85a459858
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-05-21 22:51:24 +0200

    feat: pages, navbar

--- concepts/adv/src/App.jsx
+++ concepts/adv/src/App.jsx
@@ -3,19 +3,24 @@
 // import UseWindowResizeTest from "../components/use-window-resize/UseWindowResizeTest";
 // import ScrollToBottomAndTop from "../components/scroll-bottom-top/ScrollToBottomAndTop";
 // import ScrollToSection from "../components/scroll-bottom-top/ScrollToSection";
+// import Weather from "../components/weather-app/components/weather/Weather";
 
-import Weather from "../components/weather-app/components/weather/Weather";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "../components/food-recepies/components/Navbar/Navbar";
 
 export default function App() {
   return (
     <div>
-      <h1>APP</h1>
+      <h1 className="px-18">APP</h1>
       {/* <UseFetchHookTest /> */}
       {/* <UseOutsideClickTest /> */}
       {/* <UseWindowResizeTest /> */}
       {/* <ScrollToBottomAndTop /> */}
       {/* <ScrollToSection /> */}
-      <Weather />
+      {/* <Weather /> */}
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+        <Navbar />
+      </div>
     </div>
   );
 }
