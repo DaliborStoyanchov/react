3ee60a2972e8d4ac1038134b88788ba78c9edc20
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-05-30 21:41:11 +0200

    feat: add recipe card

--- concepts/adv/components/food-recepies/components/RecipeItem/RecipeItem.jsx
+++ concepts/adv/components/food-recepies/components/RecipeItem/RecipeItem.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
 const RecipeItem = ({ item }) => {
   return (
-    <div>
-      <h2 className="px-4">RecipeItem</h2>
+    <div className="flex flex-col w-80 overflow-hidden p-5 bg-white/75 shadow-xl gap-5 border-2 rounded-2xl border-white">
+      <div className="h-40 flex justify-center overflow-hidden items-center rounded-xl ">
+        <img src={item?.image_url} alt="food image" className="block w-full" />
+      </div>
+      <div>
+        <span className="text-sm text-cyan-700 font-medium">
+          {item?.publisher}
+        </span>
+        <h3 className="font-bold text-2xl truncate text-black">
+          {item?.title}
+        </h3>
+        <Link to={`/RecipeItem/${item?.id}`}>Recipe Details</Link>
+      </div>
     </div>
   );
 };
