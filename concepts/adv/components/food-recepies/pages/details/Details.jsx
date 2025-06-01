303261567591e9c1f1f3ebfd71274a590326561a
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-06-02 00:14:33 +0200

    feat: render recipe card, css

--- concepts/adv/components/food-recepies/pages/details/Details.jsx
+++ concepts/adv/components/food-recepies/pages/details/Details.jsx
@@ -17,12 +17,51 @@ const Details = () => {
 
       const data = await response.json();
 
-      console.log(data);
+      if (data?.recipe) {
+        setRecipeDetailsData(data?.recipe);
+      }
     }
     getRecipeDetails();
   }, []);
 
-  return <div>Details</div>;
+  console.log(recipeDetailsData);
+
+  return (
+    <div className="container mx-auto py-10 grid grid-cols-1 lg:grid-cols-2 gap-10">
+      <div className=" row-start-2 lg:row-start-auto">
+        <div className="h-96 overflow-hidden rounded-xl group">
+          <img
+            src={recipeDetailsData?.image_url}
+            alt="recipe image"
+            className="w-full h-full object-cover block group-hover:scale-105 duration-300"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-3 mt-5">
+        <span className="text-sm text-cyan-700 font-medium">
+          {recipeDetailsData?.publisher}
+        </span>
+        <h3 className="font-bold text-2xl truncate text-black">
+          {recipeDetailsData?.title}
+        </h3>
+        <div>
+          <button className="p-3 px-8 rounded-lg text-sm uppercase font-medium tracking-wider mt-3 shadow-md bg-black text-white hover:cursor-pointer hover:bg-gray-700">
+            Save as favorites
+          </button>
+        </div>
+        <div>
+          <span className="text-2xl font-semibold text-black">
+            Ingredients:
+          </span>
+          <ul className="flex flex-col gap-3 mt-3">
+            {recipeDetailsData?.ingredients.map((ingredient, i) => (
+              <li key={i}>- {ingredient}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
