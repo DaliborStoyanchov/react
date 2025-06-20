e8c7c1be742ffa0a5315d8936b3f72e3ce3d8e87
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-06-20 21:07:08 +0200

    feat: e-commerc init

--- concepts/adv/src/App.jsx
+++ concepts/adv/src/App.jsx
@@ -6,10 +6,12 @@
 // import Weather from "../components/weather-app/components/weather/Weather";
 
 import { Routes, Route } from "react-router-dom";
-import Navbar from "../components/food-recepies/components/Navbar/Navbar";
-import Favorites from "../components/food-recepies/pages/favorites/Favorites";
-import Home from "../components/food-recepies/pages/home/Home";
-import Details from "../components/food-recepies/pages/details/Details";
+import Home from "../components/e-commerce/pages/home";
+import Cart from "../components/e-commerce/pages/cart";
+// import Navbar from "../components/food-recepies/components/Navbar/Navbar";
+// import Favorites from "../components/food-recepies/pages/favorites/Favorites";
+// import Home from "../components/food-recepies/pages/home/Home";
+// import Details from "../components/food-recepies/pages/details/Details";
 
 export default function App() {
   return (
@@ -20,14 +22,19 @@ export default function App() {
       {/* <ScrollToBottomAndTop /> */}
       {/* <ScrollToSection /> */}
       {/* <Weather /> */}
-      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+      {/* <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/favorites" element={<Favorites />} />
           <Route path="/recipe-item/:id" element={<Details />} />
         </Routes>
-      </div>
+      </div> */}
+      <h1>NAVBAR</h1>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
