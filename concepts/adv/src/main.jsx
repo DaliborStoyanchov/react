590335e067668c2285ce076fc2a688714e3bbf6c
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2025-07-11 12:07:02 +0200

    delete: chackra vite bug

--- concepts/adv/src/main.jsx
+++ concepts/adv/src/main.jsx
@@ -2,21 +2,23 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import React from "react";
 // import { BrowserRouter } from "react-router-dom";
 // import GlobalState from "../components/food-recepies/context/index.jsx";
 // import { Provider } from "react-redux";
-import store from "../components/e-commerce/store/store.js";
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme.js";
+// import store from "../components/e-commerce/store/store.js";
+
 // import GlobalState from "../components/expences-tracker/context/context.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <Provider store={store}>
   // <BrowserRouter>
   // <GlobalState>
-  <ChakraProvider theme={theme}>
+
+  <React.StrictMode>
     <App />
-  </ChakraProvider>
+  </React.StrictMode>
+
   // </GlobalState>
   // </BrowserRouter>
   // </Provider>
--- concepts/adv/src/theme.jsx
+++ concepts/adv/src/theme.jsx
@@ -3,10 +3,7 @@ import { extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
   colors: {
     brand: {
-      50: "#f5faff",
-      100: "#e0f2ff",
-      500: "#3182ce",
-      900: "#1a365d",
+      500: "#1a202c",
     },
   },
 });
